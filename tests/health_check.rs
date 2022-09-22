d5e0adf7960a031722defd108aab21ecc499e860
T-Sumida <ui.suto05@gmail.com>
2022-09-22 13:38:20 +0900

    [fix] fix fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,5 @@
 use std::net::TcpListener;
 
-
 #[tokio::test]
 async fn health_check_works() {
     // Arange
@@ -12,16 +11,15 @@ async fn health_check_works() {
 
     // Act
     let response = client
-    .get(&format!("{}/health_check", &address))
-    .send()
-    .await
-    .expect("Failed to execute request");
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request");
 
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
